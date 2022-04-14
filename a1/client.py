8d611b6cf17860356bfae4904d663ae023979648
Franco Marcon <francomarcon@Swapfietss-MacBook-Pro-2.local>
2022-04-14 11:27:29 +0200

    command quit

--- a1/client.py
+++ a1/client.py
@@ -9,11 +9,18 @@ client.connect(('143.47.184.219', 5378))
 done = False
 
 while not done:
-    msg_send = input() + '\n'
-    
+    msg_send = input('') + '\n'
     client.send(msg_send.encode('utf-8'))
     msg = client.recv(2048).decode('utf-8')
-    print(msg)
-    
+    if msg_send == '!quit\n':
+        done = True
+        if msg_send == '!who\n':
+            client.send('WHO\n'.encode('utf-8'))
+            if msg_send.startswith('@'):
+                client.send(f'SEND {msg_send+1}'.encode('utf-8'))
+    else:
+        print(msg)
+
+
 
 client.close()
